3dcc77188d2e1f7123ccf1edd743149a92a2aadf
Лазакович Евгений <lazakovich_e@open.ru>
2024-05-30 14:23:53 +0300

    Test commit

--- artist-promotion-platform/ViewController.swift
+++ artist-promotion-platform/ViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("test")
         view.backgroundColor = .purple
     }
 }
